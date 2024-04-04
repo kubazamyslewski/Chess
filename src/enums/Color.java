2437816518af997adf54274e32f7faffb4d805a9
alabeda1 <aleksandra.labeda.stud@pw.edu.pl>
2024-04-04 21:31:31 +0200

    unnecessary color enum removed

--- src/enums/Color.java
+++ src/enums/Color.java
@@ -1,6 +1,40 @@
 package enums;
 
+/**
+ * This enum has two colors black and white
+ * They have 2 variable colorName and symbol (which can be used in chess notation)
+ */
 public enum Color {
-    WHITE,
-    BLACK;
+    WHITE("white", 'w'),
+    BLACK("black",'b');
+
+
+    protected String colorName;
+    protected char symbol;
+    Color(String colorName, char symbol){
+        this.colorName = colorName;
+        this.symbol = symbol;
+    }
+    public String getColorName()
+    {
+        return colorName;
+    }
+
+    public char getSymbol()
+    {
+        return symbol;
+    }
+
+    public String getSymbolAsString()
+    {
+        return String.valueOf(symbol);
+    }
+
+    public void setColorName(String colorName) {
+        this.colorName = colorName;
+    }
+
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
+    }
 }
