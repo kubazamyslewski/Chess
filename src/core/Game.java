ea716f742235ddcba7af458e8c30b618dd90fe0b
Kamil Gromko <01187019@pw.edu.pl>
2024-05-08 23:41:20 +0200

    Game fix

--- src/core/Game.java
+++ src/core/Game.java
@@ -15,7 +15,7 @@ import java.util.List;
  * This class controls the game by starting it, moving pieces, determining winner
  */
 public class Game extends GameLogic {
-	
+
     private Chessboard board;
     Player playerWhite;
     Player playerBlack;
@@ -23,13 +23,34 @@ public class Game extends GameLogic {
     public Scanner systemin = new Scanner(System.in);
 
     private boolean isWhitePlayerTurn=true;
-    
-    
+
+
     public Game(){
-        newGame();
+        init();
+
+        saveGame();
+        loadGame();
+
+    }
+
+    /**
+     * for initializing all objects
+     */
+    protected final void init(){
+        board = new Chessboard();
+        board.setSquares();
+        board.setPiecesAtStart(playerWhite, playerBlack);
         setWhitePlayerTurn(true);
         setIsEnPassant(false);
-        
+
+    }
+
+
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+
+    public void newGame(){
         PlayerFactory playerFactory = new PlayerFactory();
         int type;
         String playername;
@@ -51,51 +72,6 @@ public class Game extends GameLogic {
         type = 0;
         playername = null;
         playerFactory = null;
-        
-        saveGame();
-        loadGame();
-        
-    }
-
-    /**
-     * for initializing all objects
-     */
-    protected final void init(){
-    	board = new Chessboard();
-    	board.setSquares();
-    	board.setPiecesAtStart(playerWhite, playerBlack);
-    }
-    
-    
-    public static void main(String[] args) {
-    	Game game = new Game();
-    }
-    
-    public void newGame(){
-    	init();
-    }
-    
-    public void endGame(){
-    	System.out.println("Thanks for playing");
-    }
-
-    public void moveNetworkAction(){
-
-    }
-    
-
-    /**
-     * Switches the current active player
-     */
-    public void switchActive(){
-    	isWhitePlayerTurn = !isWhitePlayerTurn;
-    }
-    
-    /**
-     * makes the lastest move in ArrayList
-     */
-    public void move(){
-
     }
 
 
@@ -108,27 +84,50 @@ public class Game extends GameLogic {
         // Get the list of moves representing the game history or the state of the board
         // Call the saveGameToFile method using the fileHandler field
         fileHandler.saveGameToFile(allMoves, board, playerBlack, playerWhite, isWhitePlayerTurn, isEnPassant, whereEnPassant);
-        
+
         fileHandler = null;
     }
 
 
     // Metoda wczytująca stan gry z pliku
     public void loadGame() {
-    	Game game = new Game();
-    	FileHandler fileHandler = new FileHandler("zapis");
-    	board = (Chessboard) fileHandler.loadGameFromFile("board.ser");
-    	allMoves = (ArrayList<Move>) fileHandler.loadGameFromFile("allMoves.ser");
-    	playerWhite = (Player) fileHandler.loadGameFromFile("playerWhite.ser");
-    	playerBlack = (Player) fileHandler.loadGameFromFile("playerBlack.ser");
-    	isWhitePlayerTurn = (boolean) fileHandler.loadGameFromFile("isWhitePlayerTurn.ser");
-    	isEnPassant = (boolean) fileHandler.loadGameFromFile("isEnPassant.ser");
-    	whereEnPassant = (Square) fileHandler.loadGameFromFile("whereEnPassant.ser");
-        
+        Game game = new Game();
+        FileHandler fileHandler = new FileHandler("zapis");
+        board = (Chessboard) fileHandler.loadGameFromFile("board.ser");
+        allMoves = (ArrayList<Move>) fileHandler.loadGameFromFile("allMoves.ser");
+        playerWhite = (Player) fileHandler.loadGameFromFile("playerWhite.ser");
+        playerBlack = (Player) fileHandler.loadGameFromFile("playerBlack.ser");
+        isWhitePlayerTurn = (boolean) fileHandler.loadGameFromFile("isWhitePlayerTurn.ser");
+        isEnPassant = (boolean) fileHandler.loadGameFromFile("isEnPassant.ser");
+        whereEnPassant = (Square) fileHandler.loadGameFromFile("whereEnPassant.ser");
+
         fileHandler = null;
     }
 
-    
+
+    public void endGame(){
+        System.out.println("Thanks for playing");
+    }
+
+    public void moveNetworkAction(){
+
+    }
+
+
+    /**
+     * Switches the current active player
+     */
+    public void switchActive(){
+        isWhitePlayerTurn = !isWhitePlayerTurn;
+    }
+
+    /**
+     * makes the lastest move in ArrayList
+     */
+    public void move(){
+
+    }
+
     public boolean isWhitePlayerTurn() {
         return isWhitePlayerTurn;
     }
@@ -136,7 +135,7 @@ public class Game extends GameLogic {
     public void setWhitePlayerTurn(boolean whitePlayerTurn) {
         isWhitePlayerTurn = whitePlayerTurn;
     }
-    
+
     public Chessboard getChessboard() {
         return board;
     }
@@ -170,5 +169,6 @@ public class Game extends GameLogic {
         this.allMoves = allMoves;
     }
 
-    
+
 }
+
